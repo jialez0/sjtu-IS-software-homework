48c66e9769d31b3ca34ab3fc734102af40ffbdac
Weishq <43176645+Weishq@users.noreply.github.com>
2020-06-21 10:04:19 +0800

    添加微信小程序校外来访人员端

---
+++ Wechat/pages/guardsPage/guard.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    markers:[
+      //示例markers
+      {
+        id:0,
+        latitude:31.01809,
+        longitude:121.43073,
+      }
+    ],
+    latitude:31.01936,
+    longitude:121.43021,
+    clock:'',
+  },
+
+  onLoad:function(options) {
+    var that = this
+    
+  },
+})
